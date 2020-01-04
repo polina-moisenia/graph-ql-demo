aff70fbe6b6d1080cee14793299e55352a4bb848
Polina <p.moysenia@gmail.com>
2020-01-04 13:36:18 +0300

    Camel case

--- Demo/Demo/Startup.cs
+++ Demo/Demo/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson.Serialization.Conventions;
 
 namespace Demo
 {
@@ -20,8 +21,11 @@ namespace Demo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            
+            var camelCaseConventionPack = new ConventionPack { new CamelCaseElementNameConvention() };
+            ConventionRegistry.Register("CamelCase", camelCaseConventionPack, type => true);
 
-            services.Configure<MoviesDatabaseConfiguration>(Configuration.GetSection("MoviesDatabaseConfiguration"));
+            services.Configure<MoviesDatabaseConfiguration>(Configuration.GetSection(nameof(MoviesDatabaseConfiguration)));
             services.AddSingleton<IMoviesService, MoviesService>();
         }
 
